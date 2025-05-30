88d715b5d4aa918eb4d243d2b9203b5707248482
benjis9 <benjaminas223@gmail.com>
2025-05-30 10:35:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ if st.session_state["authenticated"]:
     html_table = load_prompt_from_file("criteria_table.txt")
 
     evaluation_criteria_placeholder.markdown("### Evaluation Criteria", unsafe_allow_html=True)
-    components.html(html_table, height=300, width=600)
+    components.html(html_table, height=380, width=500)
 
     if uploaded_file:
         st.success("PDF uploaded. Reading content...")
